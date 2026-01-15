62f07b5dffe7123550111f9a5cea743177cc5d9a
khcmh2509 <khcmh2509@gmail.com>
2026-01-15 11:16:05 +0900

    Amuse first commit

---
+++ server/Amuze/src/main/java/com/muse/amuze/AmuzeApplication.java
@@ -0,0 +1,13 @@
+package com.muse.amuze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmuzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmuzeApplication.class, args);
+	}
+
+}
---
+++ server/Amuze/src/test/java/com/muse/amuze/AmuzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muse.amuze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmuzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
